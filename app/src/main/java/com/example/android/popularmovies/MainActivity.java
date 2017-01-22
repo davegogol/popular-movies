28dfc7c84d0e361af9ae0f38e7b2d5f7aa04ec3e
davegogol <g.davide1989@gmail.com>
2017-01-22 17:51:58 +0100

    numbers grid

--- app/src/main/java/com/example/android/popularmovies/MainActivity.java
+++ app/src/main/java/com/example/android/popularmovies/MainActivity.java
@@ -2,12 +2,37 @@ package com.example.android.popularmovies;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.GridView;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    private GridView mGridView;
+
+    private static final String[] LETTERS =
+            new String[]{"A", "B", "C", "D", "E", "F",
+                         "G", "H", "I", "L", "M", "N",
+                         "O", "P", "Q", "R", "S", "T",
+                         "U", "V", "Z"};
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        mGridView = (GridView) findViewById(R.id.grid_view);
+        ArrayAdapter<String> adapter =
+                new ArrayAdapter<String>(
+                        this, android.R.layout.simple_list_item_1, LETTERS);
+        mGridView.setAdapter(adapter);
+        mGridView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                Toast.makeText(getApplicationContext(), ((TextView) view).getText(), Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 }
