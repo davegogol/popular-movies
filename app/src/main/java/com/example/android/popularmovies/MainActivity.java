82476b36341e375d63ce162eebf9c3e790b9e213
davegogol <g.davide1989@gmail.com>
2017-01-22 17:18:54 +0100

    display image to main activity

--- app/src/main/java/com/example/android/popularmovies/MainActivity.java
+++ app/src/main/java/com/example/android/popularmovies/MainActivity.java
@@ -2,12 +2,19 @@ package com.example.android.popularmovies;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ImageView;
+
+import com.squareup.picasso.Picasso;
 
 public class MainActivity extends AppCompatActivity {
+
+    private ImageView mGridImagesView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        mGridImagesView = (ImageView) findViewById(R.id.grid_images);
+        Picasso.with(this).load("http://image.tmdb.org/t/p/w500/nBNZadXqJSdt05SHLqgT0HuC5Gm.jpg").into(mGridImagesView);
     }
 }
