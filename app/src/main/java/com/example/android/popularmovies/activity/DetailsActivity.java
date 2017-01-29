f419f74702e189a3fb7b147b0a36f2f916419826
davegogol <g.davide1989@gmail.com>
2017-01-29 23:50:13 +0100

    array adapter trasformation + details view improved

--- app/src/main/java/com/example/android/popularmovies/activity/DetailsActivity.java
+++ app/src/main/java/com/example/android/popularmovies/activity/DetailsActivity.java
@@ -3,19 +3,31 @@ package com.example.android.popularmovies.activity;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ImageView;
 import android.widget.TextView;
 import com.example.android.popularmovies.R;
+import com.squareup.picasso.Picasso;
+
 /**
  * Details Activity represents the Activity which empowers
  * the visualization of a single movie details.
  */
 public class DetailsActivity extends AppCompatActivity {
     private TextView movieTitleTextView;
+    private TextView movieOverviewTextView;
+    private TextView movieReleaseDateTextView;
+    private TextView movieAverageRateTextView;
+    private ImageView moviePosterImageView;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_details);
         movieTitleTextView = (TextView) findViewById(R.id.details_movie_title);
+        movieOverviewTextView = (TextView) findViewById(R.id.details_movie_overview);
+        movieReleaseDateTextView = (TextView) findViewById(R.id.details_movie_release_date);
+        movieAverageRateTextView = (TextView) findViewById(R.id.details_movie_average_rate);
+        moviePosterImageView = (ImageView) findViewById(R.id.movie_single_img);
         Intent intent = getIntent();
         if(intent != null && intent.hasExtra("movie.title") && intent.hasExtra("movie.poster")) {
             String title = intent.getStringExtra("movie.title");
@@ -23,7 +35,15 @@ public class DetailsActivity extends AppCompatActivity {
             String releaseDate = intent.getStringExtra("movie.release_date");
             String averageRate = intent.getStringExtra("movie.average_rate");
             String overview = intent.getStringExtra("movie.overview");
-            movieTitleTextView.setText(title + poster + releaseDate + averageRate + overview);
+            movieTitleTextView.setText(title);
+            movieOverviewTextView.setText(overview);
+            movieReleaseDateTextView.setText(releaseDate);
+            movieAverageRateTextView.setText(averageRate);
+            Picasso.
+                    with(this).
+                    load("http://image.tmdb.org/t/p/w150"+ poster).
+                    into(moviePosterImageView);
+
         }
     }
 }
