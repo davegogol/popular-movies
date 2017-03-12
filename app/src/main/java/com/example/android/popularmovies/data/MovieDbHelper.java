fdbd8f93ecf203400b95ab8f660a4c92ee48e9bf
davegogol <g.davide1989@gmail.com>
2017-03-12 01:02:20 +0100

    setting up movie content provider (query, delete, insert ops), movie contract, movie db helper

---
+++ app/src/main/java/com/example/android/popularmovies/data/MovieDbHelper.java
@@ -0,0 +1,37 @@
+package com.example.android.popularmovies.data;
+
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+/**
+ * Manages a local database for movie data.
+ */
+public class MovieDbHelper extends SQLiteOpenHelper {
+
+    private static final int DATABASE_VERSION = 1;
+    public static final String DATABASE_NAME = "movie.db";
+
+    /**
+     * Constructor.
+     * @param context
+     */
+    public MovieDbHelper(Context context) {
+        super(context, DATABASE_NAME, null, DATABASE_VERSION);
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+        final String SQL_CREATE_FAVOURITES_MOVIES_TABLE =
+                "CREATE TABLE " + MovieContract.FavouriteEntry.TABLE_NAME + " (" +
+                        MovieContract.FavouriteEntry._ID   + " INTEGER PRIMARY KEY );";
+
+        db.execSQL(SQL_CREATE_FAVOURITES_MOVIES_TABLE);
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+        db.execSQL("DROP TABLE IF EXISTS " + MovieContract.FavouriteEntry.TABLE_NAME);
+        onCreate(db);
+    }
+}
